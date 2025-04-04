2ac171b1b8c7a48d24c127b0fb8218cff55c2305
Adhil Abdulla <your-email@example.com>
2025-04-05 01:18:00 +0530

    Initial commit

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://url-shortener-gmbzd75sl-jerryy.vercel.app/api";
+const API_URL = "http://localhost:5000/api";
 
 async function shortenUrl() {
     const longUrl = document.getElementById("longUrl").value;
