49c40d9c9eaa810af3145395efddb0ba32f46dd0
Adhil Abdulla <your-email@example.com>
2025-04-05 01:22:34 +0530

    routes updated

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = `${process.env.BASE_URL}/api`;
 
 async function shortenUrl() {
     const longUrl = document.getElementById("longUrl").value;
